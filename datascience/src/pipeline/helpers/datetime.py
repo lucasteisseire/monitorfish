8dc5e355b0ebb677348bb7b37b3e9f5c2fdac818
Vincent <vincent.chery@m4x.org>
2021-06-12 18:11:13 +0200

    Add datetime chunking utils

--- datascience/src/pipeline/helpers/datetime.py
+++ datascience/src/pipeline/helpers/datetime.py
@@ -1,4 +1,79 @@
+import uuid
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from typing import List
+
 import pandas as pd
+
+
+@dataclass
+class DatetimeChunk:
+    """Class to represent a period of time between two dates, to support processing of
+    time-series data in chunks"""
+
+    id: str = field(default_factory=lambda: uuid.uuid4().hex, init=False)
+    start_datetime_utc: datetime
+    end_datetime_utc: datetime
+
+
+def get_next_chunk_end_datetime(
+    start_datetime_utc: datetime, max_datetime_utc: datetime, days_per_chunk: int
+) -> datetime:
+    """From a given datetime, computes the end datetime of the next chunk of some data
+    to process, with the following rules:
+        - the chunk starts at the indicated `start_datetime_utc`
+        - the chunk lasts `days_per_chunk` days
+        - if the end of the chunk exceeds `max_datetime_utc`, the chunk is clipped to
+        `max_datetime_utc`, and is therefore shorted than `days_per_chunk` days
+
+    Args:
+        start_datetime_utc (datetime): Starting datetime.
+        max_datetime_utc (datetime): The datetime that the chunk must not exceed.
+        days_per_chunk (int): Number of days per chunk
+
+    Returns:
+        datetime: The next chunk end datetime
+    """
+    next_chunk_end_datetime = min(
+        start_datetime_utc + timedelta(days=days_per_chunk),
+        max_datetime_utc + timedelta(seconds=1),
+    )
+
+    return next_chunk_end_datetime
+
+
+def get_datetime_chunks(
+    start_datetime_utc: datetime, end_datetime_utc: datetime, days_per_chunk: int
+) -> List[DatetimeChunk]:
+    """Returns a list of `DatetimeChunk` objects between two dates with each chunk
+    habing a duration as specified by the `days_per_chunk` argument.
+
+    Args:
+        start_datetime_utc (datetime): Start of analysis period
+        end_datetime_utc (datetime): End of analysis period
+        days_per_chunk (int): Number of days per chunk
+
+    Returns:
+        List[DatetimeChunk]: List of DatetimeChunk objects covering the specied period
+    """
+    res = []
+    utcnow = datetime.utcnow()
+    end_of_period_covered = start_datetime_utc
+
+    while end_of_period_covered < end_datetime_utc:
+        chunk_start = end_of_period_covered
+        chunk_end = get_next_chunk_end_datetime(
+            start_datetime_utc=chunk_start,
+            max_datetime_utc=utcnow,
+            days_per_chunk=days_per_chunk,
+        )
+        chunk = DatetimeChunk(
+            start_datetime_utc=chunk_start, end_datetime_utc=chunk_end
+        )
+        res.append(chunk)
+        end_of_period_covered = chunk_end
+
+    return res
 
 
 def get_datetime_index_intervals(
